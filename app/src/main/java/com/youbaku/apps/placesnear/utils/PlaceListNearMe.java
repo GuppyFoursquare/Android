41ca76dd248d556332ea42e8f010b6f89bd3c5f2
Kemal Sami <kemalsami.karaca@tubitak.gov.tr>
2015-07-18 14:27:26 +0300

    ** Youbaku.Android.001 ** ArrayList Listener added

---
+++ app/src/main/java/com/youbaku/apps/placesnear/utils/PlaceListNearMe.java
@@ -0,0 +1,23 @@
+package com.youbaku.apps.placesnear.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by kemal on 17/07/15.
+ */
+public class PlaceListNearMe<T> {
+
+
+    private Map<String,T> placesListNearMe;
+
+    public PlaceListNearMe(){
+        placesListNearMe = new HashMap<String,T>();
+    }
+
+    public void add(String key , T t){
+        placesListNearMe.put(key,t);
+        //do other things you want to do when items are added
+    }
+
+}
